3cf3d784a763dcc5e932b21608f65ccd4d8963d5
Virak Khun <virak.khun@okoone.com>
2023-10-01 22:55:06 +0700

    chore: update README.md in docs/

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
 	organizationName: 'utiliser', // Usually your GitHub org/user name.
 	projectName: 'utiliser', // Usually your repo name.
 	trailingSlash: false,
+	deploymentBranch: 'deployment',
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
