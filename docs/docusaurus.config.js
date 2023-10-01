b0b7ce0fca771e73633bafbedd3eff73e098301a
Virak Khun <virak.khun@okoone.com>
2023-10-01 18:17:03 +0700

    chore: setup ci to deploy doc to gh-page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,9 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'URH[🪝]',
+	title: 'utiliser',
 	tagline: 'The simple react hooks.',
-	url: 'https://your-docusaurus-test-site.com',
 	baseUrl: '/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
@@ -16,8 +15,9 @@ const config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'Virak Khun', // Usually your GitHub org/user name.
-	projectName: 'urh-react-hooks', // Usually your repo name.
+	url: 'https://utiliser.github.io',
+	organizationName: 'utiliser', // Usually your GitHub org/user name.
+	projectName: 'utiliser', // Usually your repo name.
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
