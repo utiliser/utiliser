637a502218f0a9da2b28479df693c84787ae671e
Virak Khun <virak.khun@okoone.com>
2023-10-01 23:32:29 +0700

    may be fixed

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
 	title: 'utiliser',
 	tagline: 'The simple react hooks.',
-	baseUrl: '/utiliser',
+	baseUrl: '/utiliser/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
