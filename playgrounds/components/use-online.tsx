9c629c901aa50aa5869834270927786cae025b6f
Virak Khun <virak.khun@okoone.com>
2023-10-01 22:27:12 +0700

    publish for the first time

--- playgrounds/components/use-online.tsx
+++ playgrounds/components/use-online.tsx
@@ -1,5 +1,5 @@
+import { useOnline } from '@utiliser/react-hooks'
 import React from 'react'
-import { useOnline } from 'urh-react-hooks'
 
 export const UseIsOnline = () => {
 	const { isOnline } = useOnline()
